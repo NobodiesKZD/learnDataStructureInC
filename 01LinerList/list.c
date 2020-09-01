2c7f71e1cae812b909d043e28f8f1f85423f0245
NobodiesKZD <simple_idea_yo@163.com>
2020-09-01 20:31:03 +0800

    顺序表代码提交

--- 01LinerList/list.c
+++ 01LinerList/list.c
@@ -18,13 +18,42 @@ int isListEmpty(SeqList L){
     }
 }
 
-int getElem(SeqList L,int i,DataType *e){
+int getElemByIndex(SeqList L,int i, DataType e){
     if(i < 0 || i > L.length){
         return -1;
     }
     else{
-        *e = L.list[i-1];
+        e = L.list[i-1];
         return 1;
     }
 }
 
+int locateElem(SeqList L, DataType e){
+    int i;
+    for(i=0; i<L.length;i++){
+        if(L.list[i] == e){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int inserList(SeqList *L, int i, DataType e){
+    if(i < 0 || i > L->list.length){
+        printf("插入位置不合法\n");
+        return -1;
+    }
+    else if(L->length >= LISTSIZE){
+        printf("list is full\n");
+        return -2;
+    }
+    else{
+        int j;
+        for(j = L->length; j > i; j--){
+            L->list[j] = L->list[j-1];
+        }
+        L->list[i] = e;
+        L->length = L->length + 1;
+        return 1;
+    }
+}
